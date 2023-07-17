274aec2d419afc8f2cf64483aacd8d95992772f6
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-07-17 10:08:43 +0330

    fixed bugs and modularized code

--- Phase01_Full-Text Search/src/main/java/Main.java
+++ Phase01_Full-Text Search/src/main/java/Main.java
@@ -2,10 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.text.MessageFormat;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
@@ -20,7 +18,7 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return new String[] { fileName, fileContent };
+        return new String[]{fileName, fileContent};
     }
 
     private static String[] tokenize(String text) {
@@ -71,7 +69,7 @@ public class Main {
         }
     }
 
-    private static HashMap<String, ArrayList<String>> createDictionay(HashMap<String, String> books) {
+    private static HashMap<String, ArrayList<String>> createDictionary(HashMap<String, String> books) {
         HashMap<String, HashSet<String>> dict = new HashMap<>();
 
         for (String title : books.keySet()) {
@@ -94,44 +92,89 @@ public class Main {
     }
 
     private static <T> ArrayList<T> toArrayList(HashSet<T> set) {
-        ArrayList<T> list = new ArrayList<>();
-        list.addAll(set);
-        return list;
+        return new ArrayList<>(set);
     }
 
-    private static void removeDuplicates(ArrayList<String> list) {
+    private static <T> void removeDuplicates(ArrayList<T> list) {
+        Set<T> set = new HashSet<>(list);
+        list.clear();
+        list.addAll(set);
+    }
 
+    private static void printQueryResult(ArrayList<String> result,long startTime) {
+        System.out.println(MessageFormat.format("{0} records found in {1}ns!", result.size(), System.nanoTime() - startTime));
+        System.out.println(result);
+    }
+    private static ArrayList<String> runQueries(HashMap<String, ArrayList<String>> queries,HashMap<String, ArrayList<String>> dictionary) {
+        ArrayList<String> result = new ArrayList<>();
+        boolean firstPart = true;
+        for (String q : queries.get("AND")) {
+            var searchResult = search(dictionary, q);
+            if (firstPart) {
+                result = searchResult;
+                firstPart = false;
+            } else {
+                result.retainAll(searchResult);
+            }
+        }
+        ArrayList<String> unionPlusResult = new ArrayList<>();
+        for (String q : queries.get("OR")) {
+            var searchResult = search(dictionary, q);
+            unionPlusResult.removeAll(searchResult);
+            unionPlusResult.addAll(searchResult);
+        }
+        if (queries.get("OR").size() > 0)
+            result.retainAll(unionPlusResult);
+        for (String q : queries.get("NOT")) {
+            var searchResult = search(dictionary, q);
+            result.removeAll(searchResult);
+        }
+        return result;
+    }
+
+    private static HashMap<String, ArrayList<String>> separateQueries(String query) {
+        HashMap<String, ArrayList<String>> queries = new HashMap<>() {{
+            put("AND", new ArrayList<String>());
+            put("OR", new ArrayList<String>());
+            put("NOT", new ArrayList<String>());
+        }};
+        String[] parts = query.split(" +");
+        for (String part : parts) {
+            switch (part.charAt(0)) {
+                case '+':
+                    var or = queries.get("OR");
+                    or.add(part.substring(1));
+                    break;
+                case '-':
+                    var not = queries.get("NOT");
+                    not.add(part.substring(1));
+                    break;
+                default:
+                    var and = queries.get("AND");
+                    and.add(part);
+                    break;
+            }
+        }
+        return queries;
+    }
+
+    private static ArrayList<String> search(HashMap<String, ArrayList<String>> dictionary, String q) {
+        var result = dictionary.get(q);
+        if (result == null)
+            return new ArrayList<String>();
+        return result;
     }
 
     public static void main(String[] args) {
         HashMap<String, String> books = getDataset(
                 "./src/main/resources/Software Books Dataset/");
 
-        HashMap<String, ArrayList<String>> dictionary = createDictionay(books);
-        String query = "goal +get";
-        String[] parts = query.split(" +");
-        ArrayList<String> result = new ArrayList<>();
-        for (String part : parts) {
-            ArrayList<String> searchResult;
-            switch (part.charAt(0)) {
-                case '+':
-                    searchResult = dictionary.get(part.substring(1));
+        HashMap<String, ArrayList<String>> dictionary = createDictionary(books);
+        String query = "goal -compiler +java +design";
 
-                    result.removeAll(searchResult);
-                    result.addAll(searchResult);
-                    break;
-                case '-':
-                    searchResult = dictionary.get(part.substring(1));
-                    result.removeAll(searchResult);
-                    break;
-
-                default:
-                    searchResult = dictionary.get(part);
-                    result.retainAll(searchResult);
-                    break;
-            }
-        }
-        System.out.println(result);
-
+        long startTime = System.nanoTime();
+        HashMap<String, ArrayList<String>> queries = separateQueries(query);
+        ArrayList<String> result =runQueries(queries,dictionary);
+        printQueryResult(result,startTime);
     }
 }
