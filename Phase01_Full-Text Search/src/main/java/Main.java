f40e1572e591d1acd601d6f7ef1c93b1c8cbd061
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-07-18 11:08:03 +0330

    delete unused import

--- Phase01_Full-Text Search/src/main/java/Main.java
+++ Phase01_Full-Text Search/src/main/java/Main.java
@@ -1,11 +1,7 @@
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        FolderPath folderPath=new FolderPath("./src/main/resources/Software Books Dataset/","./src/main/resources/stopwords.txt");
+        FolderPath folderPath = new FolderPath("./src/main/resources/Software Books Dataset/",
+                "./src/main/resources/stopwords.txt");
         new Application(folderPath).run();
     }
 }
