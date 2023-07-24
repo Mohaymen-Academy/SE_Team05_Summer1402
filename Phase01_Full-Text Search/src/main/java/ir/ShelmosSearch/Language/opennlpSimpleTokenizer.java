f3858b58b84f798ccf4ad756b1b8e35d4c6afe4f
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-07-24 11:55:58 +0330

    add lombok and getters and setters

--- Phase01_Full-Text Search/src/main/java/ir/ShelmosSearch/Language/opennlpSimpleTokenizer.java
+++ Phase01_Full-Text Search/src/main/java/ir/ShelmosSearch/Language/opennlpSimpleTokenizer.java
@@ -5,7 +5,7 @@ import opennlp.tools.tokenize.SimpleTokenizer;
 
 import java.util.ArrayList;
 
-public class opennlpSimpleTokenizer implements Tokenizer {
+public class OpennlpSimpleTokenizer implements Tokenizer {
     public ArrayList<String> tokenize(String text) {
         SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
         return new ArrayList<>(Util.toArrayList(tokenizer.tokenize(text)));
