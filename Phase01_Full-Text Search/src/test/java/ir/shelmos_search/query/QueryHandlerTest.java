f717b9b1df0b175bc5276124d75389f7092f8aa9
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-07-29 11:07:44 +0330

    refactor: improved imports and make final field

--- Phase01_Full-Text Search/src/test/java/ir/shelmos_search/query/QueryHandlerTest.java
+++ Phase01_Full-Text Search/src/test/java/ir/shelmos_search/query/QueryHandlerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import java.util.*;
+import java.util.HashMap;
 
 class QueryHandlerTest {
     private QueryHandler queryHandler;
