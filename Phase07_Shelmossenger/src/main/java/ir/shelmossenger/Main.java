de37340c06a4c69f513b5495ce5e964222a84d46
M.S.Poulaei <sadeghpoolaee@gmail.com>
2023-07-31 13:57:35 +0330

    removed redundant code in Main

--- Phase07_Shelmossenger/src/main/java/ir/shelmossenger/Main.java
+++ Phase07_Shelmossenger/src/main/java/ir/shelmossenger/Main.java
@@ -1,16 +1,9 @@
 package ir.shelmossenger;
 
-import ir.shelmossenger.context.DbContext;
-
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.println("Hello world!");
-        // Use the method we defined earlier to create a datasource
-        DbContext context = new DbContext();
-        var log = context.login("asfa", "pass");
-        System.out.println(log);
     }
 
 }
