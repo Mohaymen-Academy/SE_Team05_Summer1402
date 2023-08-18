1675a2213293d3589eb85bd16d46e0cc16217345
AmirHossein <amirhossein.n2003@gmail.com>
2023-08-18 17:44:51 +0330

    add README

--- Phase07_Shelmossenger/src/main/java/ir/shelmossenger/Main.java
+++ Phase07_Shelmossenger/src/main/java/ir/shelmossenger/Main.java
@@ -19,7 +19,7 @@ public class Main {
 //        addPermissionToUserChat();
 //        sendMessage();
 //        editMessage();
-        deleteMessage();
+//        deleteMessage();
 //        getMessagesOfUser();
 //        getNumberOfMessagesOfUSer();
 //        averageNumberOfMessages();
